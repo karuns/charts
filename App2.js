c2484c8c50040714c9dd1720b5e00be003a72f87
CodingOcean <dadwal.kartik@gmail.com>
2015-11-12 14:29:17 -0800

    commit3

--- App2.js
+++ App2.js
@@ -106,8 +106,6 @@ Ext.define('CustomApp', {
        		case "P1":
        			console.log("Loading Open Defects chart");
        			this._openDefect();
-       			console.log("Loading release chartsss");
-       			this._releaseContainer();
        			break;
        		case "P2":
        			console.log("Loading chart2");
@@ -134,8 +132,9 @@ Ext.define('CustomApp', {
                         report: Rally.ui.report.StandardReport.Reports.Throughput,
                         work_items: 'G,D',
                         filter_field: 'ScheduleState',
-                        from_state: 'Accepted',
-                        to_state: 'Accepted'
+//                        from_state: 'Accepted',
+//                        to_state: 'Accepted',
+//                        DefectsByPriority: 'P1'
                     }
     	});
     },
@@ -148,12 +147,13 @@ Ext.define('CustomApp', {
         renderTo: Ext.getBody().dom,
 	    listeners: {
             ready: this._loadReleaseChart(this.relContainer.getValue()),
-            select: this._loadReleaseChart(this.relContainer.getValue()),
+           select: this._loadReleaseChart("8.1"),
             scope: this
         }
     	});
     	//console.log(this.relContainer.getValue());
     	this.chartContainer.add(this.relContainer);
+    	console.log(this.relContainer.getRecord().get('Name'));
     	
     },
     
@@ -350,6 +350,8 @@ Ext.define('CustomApp', {
     	console.log(this._getStoreConfig());
     	console.log(this._getStoreConfig().valueOf());
     	this.chartContainer.add(this.chart);
+		console.log("Loading release chartsss");
+		this._releaseContainer();
     },
     
     _getStoreForopenDefect: function() {
@@ -360,10 +362,8 @@ Ext.define('CustomApp', {
                 _ProjectHierarchy: this.getContext().getProject().ObjectID,
                 _ValidFrom: {'$gt': Rally.util.DateTime.toIsoString(Rally.util.DateTime.add(new Date(), 'day', -120)) },
                 State: "Open",
-                //SubmittedBy: "Customer Support",
+                //Release: "8.0"
             },
-//            fetch: ['ScheduleState'],
-//            hydrate: ['ScheduleState'], 
             fetch: ['Severity','Release','Project'],
             hydrate: ['Severity','Release','Project'],
             sort: {
@@ -580,236 +580,7 @@ Ext.define('CustomApp', {
 	            }
 	        };
 	    }
-    
-//    /* Load PI burnup */
-//    _loadPiBurnups : function() {
-//        	
-//    	this.chart = {
-//                xtype: 'rallychart',
-//                loadMask: true,
-//                chartData: this._getChartData(),
-//                chartConfig: this._getChartConfig()
-//            },
-//            this.chartContainer.add(this.chart);
-//    },
-//    
-//    _getChartData: function() {
-//        return {
-//            categories: [
-//                'Jan',
-//                'Feb',
-//                'Mar',
-//                'Apr',
-//                'May',
-//                'Jun',
-//                'Jul',
-//                'Aug',
-//                'Sep',
-//                'Oct',
-//                'Nov',
-//                'Dec'
-//            ],
-//            series: [
-//                {
-//                    name: 'Tokyo',
-//                    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-//
-//                },
-//                {
-//                    name: 'New York',
-//                    data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-//
-//                },
-//                {
-//                    name: 'London',
-//                    data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-//
-//                },
-//                {
-//                    name: 'Berlin',
-//                    data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-//
-//                }
-//            ]
-//        };
-//    },
-//
-//    /**
-//     * Generate a valid Highcharts configuration object to specify the column chart
-//     */
-//    _getChartConfig: function() {
-//        return {
-//            chart: {
-//                type: 'column'
-//            },
-//            title: {
-//                text: 'Monthly Average Rainfall'
-//            },
-//            subtitle: {
-//                text: 'Source: WorldClimate.com'
-//            },
-//            xAxis: {
-//            },
-//            yAxis: {
-//                min: 0,
-//                    title: {
-//                    text: 'Rainfall (mm)'
-//                }
-//            },
-//            tooltip: {
-//                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-//                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-//                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-//                    footerFormat: '</table>',
-//                    shared: true,
-//                    useHTML: true
-//            },
-//            plotOptions: {
-//                column: {
-//                    pointPadding: 0.2,
-//                        borderWidth: 0
-//                }
-//            }
-//        };
-//    },
-
-//    ////   ---- OLD Stuff 
-//
-//    _loadIterations: function () {
-//      this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox' , {
-//        fieldLabel:'Iterationss',
-//        labelAlign:'right',
-//
-//        listeners: {
-//            ready: function(combobox) {
-//                this._loadSeverity();
-//            },
-//            select: function(combobox) {
-//            this._loadData();
-//            },
-//            scope: this
-//        }
-//      });
-//      this.pulldownContainer.add(this.iterComboBox);
-//    },
-//
-//    _loadSeverity: function () {
-//        this.severityComboBox = Ext.create('Rally.ui.combobox.FieldValueComboBox' , {
-//            fieldLabel:'Severity',
-//            labelAlign:'right',
-//
-//            model:'Defect',
-//            field:'Severity',
-//            listeners: {
-//                ready: function(combobox) {
-//                    this._loadData();
-//                },
-//                select: function(combobox) {
-//                    this._loadData();
-//                },
-//                scope: this
-//            }
-//        });
-//        this.pulldownContainer.add(this.severityComboBox);
-//    },
-//
-//
-//
-//    _loadData: function() {
-//      var selectedIterationRef = this.iterComboBox.getRecord().get("_ref");
-//      var selectedSeverityValue = this.severityComboBox.getRecord().get('value');
-//      console.log("val=",selectedSeverityValue);
-//
-//      var myFilter = [
-//            {
-//                property:'Iteration',
-//                operation:'=',
-//                value:selectedIterationRef
-//            },
-//            {
-//                property:'Severity',
-//                operation:'=',
-//                value: selectedSeverityValue
-//            }
-//        ]
-//    // if no defect store then creat
-//    if(this.myDefectStore) {
-//        this.myDefectStore.setFilter(myFilter);
-//        this.myDefectStore.load();
-//    }
-//    else {
-//        // set filter and load
-//        this.myDefectStore = Ext.create('Rally.data.wsapi.Store', {
-//            model: 'Defect',
-//            autoLoad: true,                         // <----- Don't forget to set this to true! heh
-//            filters: myFilter,
-//            listeners: {
-//                load: function (myStore, myData, success) {
-//                    console.log('got data!', myStore, myData)
-//                    if(!this.myGrid) {
-//                        this._createGrid(myStore);
-//                    }// if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
-//                },
-//                scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
-//            },
-//            fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']   // Look in the WSAPI docs online to see all fields available!
-//        });
-//    }
-//
-//    },
-//
-//
-//    _createGrid: function(myStoryStore) {
-//
-//      this.myGrid = Ext.create('Rally.ui.grid.Grid', {
-//        store: myStoryStore,
-//        columnCfgs: [         // Columns to display; must be the same names specified in the fetch: above in the wsapi data store
-//          'FormattedID', 'Name', 'Severity', 'Iteration'
-//        ]
-//      });
-//
-//      this.add(this.myGrid);       // add the grid Component to the app-level Container (by doing this.add, it uses the app container)
-//      console.log('what is this?', this);
-//
-//    }
-    
-//  /* Load burn down chart */
-//  _loadBurnDownChart : function() {
-//  	this.chart = {
-//              xtype: 'rallychart',
-//              project: Rally.util.Ref.getRelativeUri(this.getContext().getProject()),
-//              projectScopeUp: this.getContext().getProjectScopeUp(),
-//              projectScopeDown: this.getContext().getProjectScopeDown(),
-//              width: 600,
-//              height: 400,
-//              storeConfig: {
-//                  report: Rally.ui.report.StandardReport.Reports.Throughput,
-//                  work_items: 'G,D',
-//                  filter_field: 'ScheduleState'
-//              }
-//          },
-//  	
-//  	this.chartContainer.add(this.chart);
-//  },
-	    //* Load individual charts *//    
-//	    _getData : function () {
-//	        // set filter and load
-//	        this.myDefectStore = Ext.create('Rally.data.wsapi.Store', {
-//	            model: 'Defect',
-//	            autoLoad: true,                         // <----- Don't forget to set this to true! heh
-//	            filters: myFilter,
-//	            listeners: {
-//	                load: function (myStore, myData, success) {
-//	                    console.log('got data!', myStore, myData)
-//	                    if(!this.myGrid) {
-//	                        this._createGrid(myStore);
-//	                    }// if we did NOT pass scope:this below, this line would be incorrectly trying to call _createGrid() on the store which does not exist.
-//	                },
-//	                scope: this                         // This tells the wsapi data store to forward pass along the app-level context into ALL listener functions
-//	            },
-//	            fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']   // Look in the WSAPI docs online to see all fields available!
-//	        });
-//	    },
+ 
 
 });
 
